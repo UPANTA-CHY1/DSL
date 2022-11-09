d49712c4b0d57f5815761c059506ae5314a22a0b
UPANTA-CHY1 <114695072+UPANTA-CHY1@users.noreply.github.com>
2022-11-10 00:53:20 +0600

    Add files via upload

---
+++ mid assignment1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[10],sum,x,y;
+    cout<<"Enter 10 Elements: ";
+    for(int i=0; i<10; i++)
+        cin>>arr[i];
+    int min_sum =arr[0]+arr[1];
+
+    for(int i=0; i<10-1; i++)
+        for(int j=i+1; j<10; j++)
+        {
+            sum=arr[i]+arr[j];
+
+            if(abs(min_sum)>abs(sum))
+            {
+                min_sum=sum;
+                x=i;
+                y=j;
+            }
+        }
+
+    cout<<"\nTwo Elements Index: "<<a<<" & "<<b<<"\nTheir Sum Close to Zero is : "<<min_sum;
+
+}
+
